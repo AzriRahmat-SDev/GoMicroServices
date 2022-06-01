7791095881dcebfc05b3a4bc01edeaca07e8d11e
Azri Rahmat <azri-rahmat@Azris-MacBook-Pro.local>
2022-06-01 19:43:56 +0800

    Minor fixes

--- pkg/routes/routes.go
+++ pkg/routes/routes.go
@@ -9,7 +9,7 @@ import (
 var CourseRoutes = func(router *mux.Router) {
 
 	router.HandleFunc("/api/v1/", controller.Home)
-	router.HandleFunc("/api/v1/courses", controller.GetCourse)                                              //need to display the courses in the DB
-	router.HandleFunc("/api/v1/courses/{courseid}/{title}", controller.CreateCourse).Methods("PUT", "POST") //routes and attaching a CRUD request
-	router.HandleFunc("/api/v1/courses/{courseid}", controller.DeleteCourse).Methods("DELETE")              //routes and attaching a CRUD request
+	router.HandleFunc("/api/v1/courses", controller.GetCourse)                                          //need to display the courses in the DB
+	router.HandleFunc("/api/v1/courses/add/{courseId}", controller.CreateCourse).Methods("PUT", "POST") //routes and attaching a CRUD request
+	router.HandleFunc("/api/v1/courses/delete/{courseId}", controller.DeleteCourse).Methods("DELETE")   //routes and attaching a CRUD request
 }
